8d00fc6872b7e5102c37ecc6374a969b73828a4d
Adrien Joly <adrien@whyd.com>
2014-01-15 15:45:32 +0100

    added console title + set current directory

--- test/server.sh
+++ test/server.sh
@@ -1,2 +1,8 @@
+# set mac osx's terminal title
+echo -n -e "\033]0;playemJS test server\007"
+
+# make sure to switch to the script's dir (e.g. when launched via mac osx's finder)
+cd `dirname "$0"`
 cd ..
+
 python -m SimpleHTTPServer
