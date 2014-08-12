6886fa07d9ea12781da41b5a92233faa2454b920
Adrien Joly <adrien.joly@gmail.com>
2014-08-12 15:43:25 +0200

    added TestUI => html display of progress + bug fix in until() method: keep waiting for event while callback function returns true

--- test/test.js
+++ test/test.js
@@ -60,8 +60,6 @@ function PlayemLoader() {
 
 function PlayemLogger() {
 
-	var LOG_PLAYER_EVENTS = false;
-
 	var EVENTS = [
 		"onError",
 		"onReady",
@@ -79,23 +77,22 @@ function PlayemLogger() {
 	var listeners = [];
 	var lastTypedEvent = {};
 
-	function makeLogger(evt){
+	function makeLogger(evt, handler){
 		return function(){
 			var entry = [ Date.now(), evt ].concat(Array.prototype.slice.call(arguments));
 			self.log.push(entry);
 			lastTypedEvent[evt] = entry;
-			if (LOG_PLAYER_EVENTS)
-				console.log.apply(console, /*entry*/[ Date.now(), evt ]);
+			handler && handler(entry);
 			for(var i in listeners)
 				listeners[i](evt, arguments);
 			return entry;
 		};
 	}
-	this.listenTo = function(playem){
+	this.listenTo = function(playem, handler){
 		var handlers = {};
 		for (i in EVENTS) {
 			var evt = EVENTS[i];
-			playem.on(evt, makeLogger(evt));
+			playem.on(evt, makeLogger(evt, handler));
 		}
 		playem.on("onError", function(e){
 			console.warn("catched error", e);
@@ -123,26 +120,91 @@ function PlayemLogger() {
 			that.removeListener(listenerId);
 		}
 		timeout = setTimeout(function(){
-			clean();
-			(timeoutCb || cb)();
+			// if the callbacks returns true, keeping waiting for the event
+			if (!(timeoutCb || cb)())
+				clean();
 		}, timeoutDelay);
 		listenerId = this.addListener(function(evt, data){
 			if (evtNames[evt])
+				// if the callbacks returns true, keeping waiting for the event
 				if (!cb(evt, data))
 					clean();
 		});
 	};
 }
 
-function TestRunner() {
+function PlayemWrapper(playem, logger) {
+	function wrapMethod(fctName){
+		return function(){
+			logger(fctName, arguments);
+			return playem[fctName].apply(playem, arguments);
+		};
+	}
+	for (var i in playem)
+		if (typeof playem[i] == "function")
+			this[i] = wrapMethod(i);
+	return this;
+}
+
+function TestUI(nbTracks, nbCommonTests){
+	var self = this, trackNumber = 0, testNumber = 0;
+	["eventLog", "testName", "progress", "trackUrl", "trackId", "playerName", "duration", "position"].map(function(field){
+		self[field] = document.getElementById(field);
+	});
+	this.prependToLog = function(html){
+		this.eventLog.innerHTML = html + "<br>" + this.eventLog.innerHTML;
+	};
+	this.set = function(field, html){
+		this[field].innerHTML = html || "";
+	};
+	function onTrackChange(t){
+		console.log("-> track", t.index, t.playerName, t.trackId);
+		testNumber = 0;
+		self.set("eventLog");
+		self.set("progress", ++trackNumber + " / " + nbTracks);
+		self.set("trackUrl", t.metadata.url);
+		self.set("trackId", t.trackId);
+		self.set("playerName", t.playerName);
+	};
+	function onTrackInfo(t){
+		self.set("duration", t.trackDuration);
+		self.set("position", t.trackPosition);
+	};
+	this.onNewTest = function(title){
+		self.set("testName", "(" + ++testNumber + " / " + nbCommonTests + ") " + title);
+	};
+	this.onPlayerEvent = function(entry){
+		//console.log(entry[1]);
+		self.prependToLog("-> " + entry[1]);
+	};
+	this.wrapPlayem = function(playem){
+		var playem = new PlayemWrapper(playem, function(fctName, arg){
+			self.prependToLog("<- " + fctName + " " + arg[0]);
+		});
+		playem.on("onTrackChange", onTrackChange);
+		playem.on("onTrackInfo", onTrackInfo);
+		return playem;
+	};
+	return this;
+}
+
+
+function TestRunner(options) {
 
 	var tests = [];
 	var finalCallback = null;
+	options = options || {};
+
+	function emit(evtName, p){
+		setTimeout(options[evtName] && options[evtName].bind(null, p));
+	}
 
 	function wrapTest(testFct, title){
 		return function(nextTestFct){
+			emit("onNewTest", title);
 			console.log("%c[TEST] " + title + " ...", "color:#888");
 			testFct(function(res){
+				emit("onTestResult", !!res);
 				console.log('%c[TEST]=> ' + (!!res ? "OK" : "FAIL: " + title), "color:" + (!!res ? "green" : "red"));
 				if (!!res)
 					setTimeout(nextTestFct);
