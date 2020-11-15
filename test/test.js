0cf2af9b8c8d3d46906faa87c48cbd4547c11165
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2020-11-15 13:33:19 +0100

    tests: (partially) add SoundCloud

--- test/test.js
+++ test/test.js
@@ -4,20 +4,33 @@ const assert = require('assert');
 // define globals required by players
 $ = {}; 
 document = {};
-window = { $, document };
+window = {
+  $,
+  document,
+  SC: { initialize: () => {} }, // for soundcloud
+};
 
 // load players
 const players = [
   { id: "bc", name: "Bandcamp", Player: require('./../playem-bandcamp.js') },
+  { id: "sc", name: "Soundcloud", Player: require('./../playem-soundcloud.js') },
 ];
 
 const URLS_FILE = './test/test-detection/urls.txt';
 
 const lines = fs.readFileSync(URLS_FILE, 'utf8').split('\n');
 
-describe('Id extraction', function() {
-
+describe('Player instanciation', function() {
   for (const player of players) {
+    it(`works for ${player.name}`, () => {
+      it(player.name, () => assert((new player.Player())));
+    });
+  }
+});
+
+describe('Id extraction', function() {
+  for (const player of players) {
+    if (player.name === "Soundcloud") continue; // TODO: re-activate
     describe(`works for ${player.name} URLs`, () => {
       for (const line of lines) {
         const url = line.length && line[0] != "#" && line.split(/\s/)[0];
@@ -27,5 +40,4 @@ describe('Id extraction', function() {
       }
     });
   }
-
 });
